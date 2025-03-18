445051c3678577e788243aa01cc346130eca6253
CH2344 <24110344@iitgn.ac.in>
2025-03-18 19:26:19 +0530

    Made the app version 1

--- sandbox/app_v1.py
+++ sandbox/app_v1.py
@@ -1,1 +1,15 @@
 import streamlit as st
+import torch 
+import matplotlib.pyplot as plt
+st.title('PyTorch Distributions')
+
+mu = st.slider(label='Mean',min_value=-10,max_value=10,value=1)
+sigma = st.slider(label='Variance',min_value=0.1,max_value=10.0,value=1.0)
+normal_dist = torch.distributions.Normal(mu, sigma)
+
+x_range = torch.linspace(-10, 10, 1000)
+pdf = normal_dist.log_prob(x_range).exp()
+plt.plot(x_range.numpy(), pdf.numpy())
+plt.xlabel('x')
+plt.ylabel('Probability Density')
+st.pyplot(plt)
